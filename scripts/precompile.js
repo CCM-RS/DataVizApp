32b924514fe69fa8b9ba66511ebe798238616015
CCM-RS <paul.michalet+ccm-rs@gmail.com>
2021-01-28 16:24:25 -0300

    feat: implement data conversion from Sigmine KMZ file

--- scripts/precompile.js
+++ scripts/precompile.js
@@ -1,13 +1,141 @@
 /**
  * Runs all compilation tasks that produce the necessary "intermediary" files
  * for static site generation.
+ *
+ * WIP periodically update local copy of http://sigmine.dnpm.gov.br/sirgas2000/RS.kmz
+ * (via https://www.gov.br/anm/pt-br/assuntos/acesso-a-sistemas/sistema-de-informacoes-geograficas-da-mineracao-sigmine)
+ * then parse + convert using https://github.com/mapbox/togeojson
+ * for display with https://github.com/anoram/leaflet-svelte
+ * see https://leaflet.anoram.com/examples/dynamicmarker
  */
 
-// Debug.
-console.log("precompile.js");
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const { pipeline } = require('stream');
+const tj = require('@mapbox/togeojson');
+const { DOMParser } = require('xmldom');
+const extract = require('extract-zip');
+const { write_file } = require('./lib/fs.js');
 
-// TODO periodically update local copy of http://sigmine.dnpm.gov.br/sirgas2000/RS.kmz
-// (via https://www.gov.br/anm/pt-br/assuntos/acesso-a-sistemas/sistema-de-informacoes-geograficas-da-mineracao-sigmine)
-// then parse + convert using https://github.com/mapbox/togeojson
-// for display with https://github.com/anoram/leaflet-svelte
-// see https://leaflet.anoram.com/examples/dynamicmarker
+const rsKmzFilePath = 'private/RS.kmz';
+
+/**
+ * Fetches KMZ source file for RS only if local file is older than 31 days.
+ */
+const fetchKmzFile = filePath => {
+	let requireDownload = false;
+	let fileHasBeenUpdated = false;
+	const ageLimit = 1000 * 60 * 60 * 24 * 31;
+
+	if (!fs.existsSync(filePath)) {
+		requireDownload = true;
+	} else {
+		fs.stat(filePath, (err, stat) => {
+			if (err) {
+				return console.error('Unable to stat local file ' + filePath, err);
+			}
+
+			const now = new Date().getTime();
+			const lastModif = new Date(stat.mtime).getTime();
+
+			if (now > ageLimit + lastModif) {
+				requireDownload = true;
+			} else {
+				console.log(`No need to re-download '${filePath}' because it is less than 31 days old.`);
+			}
+		});
+	}
+
+	// Nothing to do here if download is not required -> exit early.
+	if (!requireDownload) {
+		return;
+	}
+
+	// Delete obsolete version (if it exists).
+	if (fs.existsSync(filePath)) {
+		fs.unlink(filePath, err => {
+			if (err) {
+				console.error('Failed to remove the old KMZ local copy.', err);
+			}
+		});
+	}
+
+	// Download the new, up to date version.
+	// @see https://stackoverflow.com/a/60684836/2592338
+	const fileWriteStream = fs.createWriteStream(filePath);
+	http.get('http://sigmine.dnpm.gov.br/sirgas2000/RS.kmz', response => {
+		pipeline(response, fileWriteStream, err => {
+			if (err) {
+				console.error('Failed to download the KMZ file from SIGMINE.', err);
+				fs.unlink(filePath, err => {
+					if (err) {
+						console.error('Failed to remove the fileWriteStream placeholder file.', err);
+					}
+				});
+			} else {
+				fileHasBeenUpdated = true;
+				console.log('Successfully downloaded the KMZ file from SIGMINE.');
+			}
+		});
+	});
+
+	return fileHasBeenUpdated;
+}
+
+/**
+ * Extracts the KMZ source file and transforms it to geoJson format.
+ */
+const transformKmzFile = async filePath => {
+	// Delete any potentially obsolete local unzipped copies (if they exist).
+	const filesToClean = ['private/doc.kml', 'private/legend0.png'];
+	filesToClean.forEach(f => {
+		if (fs.existsSync(f)) {
+			fs.unlink(f, err => {
+				if (err) {
+					console.error(`Failed to remove the old '${f}' local copy.`, err);
+				}
+			});
+		}
+	});
+
+	// Unzip KMZ to KML.
+	try {
+		await extract(filePath, { dir: path.resolve('private') });
+		console.log('Successfully extracted the KMZ file.');
+	} catch (err) {
+		console.error('Failed to unzip the KMZ file.', err);
+		return false;
+	}
+
+	// Transform to GeoJson format.
+	const kml = new DOMParser().parseFromString(fs.readFileSync('private/doc.kml', 'utf8'));
+	if (!kml) {
+		console.error('Failed to parse KML file.', err);
+		return false;
+	}
+
+	const converted = tj.kml(kml);
+	if (!converted) {
+		console.error('Failed to convert KML file.', err);
+		return false;
+	}
+
+	// Remove potentially obsolete cached processed file.
+	if (fs.existsSync('static/data/cache/geo/RS.json')) {
+		fs.unlink('static/data/cache/geo/RS.json', err => {
+			if (err) {
+				console.error(`Failed to remove the old 'static/data/cache/geo/RS.json' cached processed file.`, err);
+			}
+		});
+	}
+
+	await write_file('static/data/cache/geo/RS.json', JSON.stringify(converted));
+}
+
+// Debug (WIP test local).
+// if (fetchKmzFile(rsKmzFilePath)) {
+// 	await transformKmzFile(rsKmzFilePath);
+// }
+
+transformKmzFile(rsKmzFilePath);
