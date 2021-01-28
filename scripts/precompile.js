e569f97103dc5e88a973e796a1390223d0a1bc3f
CCM-RS <paul.michalet+ccm-rs@gmail.com>
2021-01-28 11:41:19 -0300

    chore: update Netlify CMS config

--- scripts/precompile.js
+++ scripts/precompile.js
@@ -5,3 +5,9 @@
 
 // Debug.
 console.log("precompile.js");
+
+// TODO periodically update local copy of http://sigmine.dnpm.gov.br/sirgas2000/RS.kmz
+// (via https://www.gov.br/anm/pt-br/assuntos/acesso-a-sistemas/sistema-de-informacoes-geograficas-da-mineracao-sigmine)
+// then parse + convert using https://github.com/mapbox/togeojson
+// for display with https://github.com/anoram/leaflet-svelte
+// see https://leaflet.anoram.com/examples/dynamicmarker
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,9 +9,16 @@
 
 <svelte:head>
 	<title>{ global_data.site_name }</title>
+	<meta property="og:title" content="{ homepage.title }">
+	<meta property="og:desc" content="{ homepage.desc }">
+	<meta property="og:image" content="{ 'process.env.BASE_URL' + homepage.image ? homepage.image : '/theme/chouette-logo-1200x630.png' }">
+	<meta property="og:url" content="{ 'process.env.BASE_URL' + homepage.path ? homepage.path : '' }">
+	<meta name="twitter:card" content="{ homepage.twitter_card ? homepage.twitter_card : 'summary_large_image' }">
+	<meta property="og:site_name" content="{ global_data.site_name }">
+	<meta name="twitter:image:alt" content="{ homepage.image_alt ? homepage.image_alt : global_data.site_name }">
 </svelte:head>
 
 <div>
 	<h1>{ homepage.title }</h1>
-	<p>wip</p>
+	<p>{ homepage.desc }</p>
 </div>
